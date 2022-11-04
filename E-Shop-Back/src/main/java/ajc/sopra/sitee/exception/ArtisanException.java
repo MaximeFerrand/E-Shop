5297b8843a48add9e1330e11b3f15e31093b337e
Maxime Ferrand <ferrand.max12@gmail.com>
2022-11-04 16:31:54 +0100

    service basket / review

--- E-Shop-Back/src/main/java/ajc/sopra/sitee/exception/ArtisanException.java
+++ E-Shop-Back/src/main/java/ajc/sopra/sitee/exception/ArtisanException.java
@@ -1,6 +1,11 @@
 package ajc.sopra.sitee.exception;
 
 public class ArtisanException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ArtisanException() {
 
 	}
