893d93c991841072b4df1ca1ea9e111cfa7346aa
Jacques Tessier <jacques.tessier17@yahoo.com>
2022-11-04 15:48:16 +0100

    Services Jack

--- E-Shop-Back/src/main/java/ajc/sopra/sitee/repository/SupplierRepository.java
+++ E-Shop-Back/src/main/java/ajc/sopra/sitee/repository/SupplierRepository.java
@@ -1,10 +1,16 @@
 package ajc.sopra.sitee.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-import ajc.sopra.sitee.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
 import ajc.sopra.sitee.model.Supplier;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
-
+	@Query("select s from Supplier s left join fetch s.products where s.id=:id")
+	Optional<Supplier> findByIdFetchingProduits(@Param("id") Integer id);
 }
+
---
+++ E-Shop-Back/src/main/java/ajc/sopra/sitee/service/UserService.java
@@ -0,0 +1,29 @@
+package ajc.sopra.sitee.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ajc.sopra.sitee.model.User;
+import ajc.sopra.sitee.repository.UserRepository;
+
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepo;
+
+	public List<User> findAll() {
+		return userRepo.findAll();
+	}
+
+	public Optional<User> findById(Integer id) {
+		return userRepo.findById(id);//.orElseThrow(IdException::new);
+	}
+
+	public User save(User client) {
+		return userRepo.save(client);
+	}
+}
