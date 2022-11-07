c1e1df53b8e2c36cbd4eb9a93216e3646ea95b65
Maxime Ferrand <ferrand.max12@gmail.com>
2022-11-07 11:01:28 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- E-Shop-Back/src/main/java/ajc/sopra/sitee/model/JsonViews.java
+++ E-Shop-Back/src/main/java/ajc/sopra/sitee/model/JsonViews.java
@@ -30,12 +30,49 @@ public class JsonViews {
 	public static class OrderDetailWithReview extends Common {
 
 	}
+	
+<<<<<<< HEAD
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public class MerchantWithProduit extends Common{
+=======
+	public static class SupplierWithProduit extends Common {
+   public static class ProductWithSupplier extends Common{
+   }
 	public static class SupplierWithProduit extends Common {
    public static class ProductWithSupplier extends Common{
    }
 
+	
+	public static class UserWithAdress extends Common {
+
 	}
+	
+	public static class UserWithOrder extends Common {
+
+	}
+>>>>>>> develop
+
+	}
+	
 	public class ArtisanWithProduit extends Common{
 	}
 	public static class UserWithAdress extends Common {
