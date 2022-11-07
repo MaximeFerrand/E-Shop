68c261b573a28d65fd9de7ef02a5ca147af032a3
Maxime Ferrand <ferrand.max12@gmail.com>
2022-11-07 11:05:01 +0100

    errors fixed in jsonview

--- E-Shop-Back/src/main/java/ajc/sopra/sitee/model/JsonViews.java
+++ E-Shop-Back/src/main/java/ajc/sopra/sitee/model/JsonViews.java
@@ -3,82 +3,46 @@ package ajc.sopra.sitee.model;
 
 public class JsonViews {
 
-
-
-	
-
 	public static class Common {
-
 	}
 
 	public static class OrderWithOrderDetail extends Common {
-
 	}
 
 	public static class OrderWithUser extends Common {
-
 	}
 
 	public static class Order extends Common {
-
 	}
 
 	public static class OrderDetailWithProduct extends Common {
-
 	}
 
 	public static class OrderDetailWithReview extends Common {
+	}
 
+	public class MerchantWithProduit extends Common{
+	}
+
+	public static class SupplierWithProduit extends Common {
 	}
 	
-<<<<<<< HEAD
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public class MerchantWithProduit extends Common{
-=======
-	public static class SupplierWithProduit extends Common {
-   public static class ProductWithSupplier extends Common{
+   public static class ProductWithSupplier extends Common{  
    }
-	public static class SupplierWithProduit extends Common {
-   public static class ProductWithSupplier extends Common{
-   }
+   
+	public static class SupplierWithProduct extends Common {
+	}
+	
 
-	
 	public static class UserWithAdress extends Common {
-
 	}
 	
 	public static class UserWithOrder extends Common {
-
-	}
->>>>>>> develop
 
 	}
 	
 	public class ArtisanWithProduit extends Common{
 	}
-	public static class UserWithAdress extends Common {
-	}
-	public static class UserWithOrder extends Common {
-	}
-
+	
 
 }
