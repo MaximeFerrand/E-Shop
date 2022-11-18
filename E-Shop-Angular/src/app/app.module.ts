6cc240555c84c940a12c8fac7713a25bd8cfe740
Maxime Ferrand <ferrand.max12@gmail.com>
2022-11-18 09:46:49 +0100

    e-shop angular

---
+++ E-Shop-Angular/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './eshop/component/header-footer/header/navigation/navigation.component';
+import { HomeComponent } from './eshop/component/home/home.component';
+import { PromotionComponent } from './eshop/component/header-footer/header/promotion/promotion.component';
+import { ConnexionComponent } from './eshop/component/header-footer/header/connexion/connexion.component';
+import { HeaderComponent } from './eshop/component/header-footer/header/header.component';
+import { WelcomeComponent } from './eshop/component/home/welcome/welcome.component';
+import { NavigationHomeComponent } from './eshop/component/home/navigation-home/navigation-home.component';
+import { BestsellersComponent } from './eshop/component/home/bestsellers/bestsellers.component';
+import { ConfortComponent } from './eshop/component/home/confort/confort.component';
+import { PresentationComponent } from './eshop/component/home/presentation/presentation.component';
+import { MarquesComponent } from './eshop/component/home/marques/marques.component';
+import { FooterComponent } from './eshop/component/header-footer/footer/footer.component';
+import { SocialMediaComponent } from './eshop/component/header-footer/social-media/social-media.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    PromotionComponent,
+    ConnexionComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    NavigationHomeComponent,
+    BestsellersComponent,
+    ConfortComponent,
+    PresentationComponent,
+    MarquesComponent,
+    FooterComponent,
+    SocialMediaComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ E-Shop-Angular/src/app/eshop/component/home/confort/confort.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfortComponent } from './confort.component';
+
+describe('ConfortComponent', () => {
+  let component: ConfortComponent;
+  let fixture: ComponentFixture<ConfortComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConfortComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConfortComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
