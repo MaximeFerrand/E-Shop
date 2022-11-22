0cb77e37fb523494cd1529ad041931009d2613b6
Jacques Tessier <jacques.tessier17@yahoo.com>
2022-11-22 14:28:34 +0100

    Supplier modifV0.5

--- E-Shop-Angular/src/app/eshop/services/supplier.service.ts
+++ E-Shop-Angular/src/app/eshop/services/supplier.service.ts
@@ -1,13 +1,62 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Supplier } from '../model/supplier';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupplierService {
-  supplierToJson(arg0: Supplier) {
-    throw new Error('Method not implemented.');
+  static URL: string = 'http://localhost:8080/eshop/api/supplier';
+
+  constructor(
+    private httpClient: HttpClient,
+    private productSrv: ProductService,
+  ) {}
+
+  public findAll(): Observable<Supplier[]> {
+    return this.httpClient.get<Supplier[]>(SupplierService.URL);
   }
 
-  constructor() {}
+  public findById(id: number): Observable<Supplier> {
+    return this.httpClient.get<Supplier>(`${SupplierService.URL}/${id}`);
+  }
+
+  public deleteById(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${SupplierService.URL}/${id}`);
+  }
+
+  public update(supplier: Supplier): Observable<Supplier> {
+    return this.httpClient.put<Supplier>(
+      `${SupplierService.URL}/${supplier.id}`,
+      this.supplierToJson(supplier)
+    );
+  }
+  public create(supplier: Supplier): Observable<Supplier> {
+    return this.httpClient.post<Supplier>(
+      SupplierService.URL,
+      this.supplierToJson(supplier)
+    );
+  }
+
+  public supplierToJson(supplier: Supplier): any {
+    let supplierJson = {
+      login: supplier.login,
+      company: supplier.company,
+    };
+    if (supplier.products) {
+      Object.assign(supplierJson, {
+        product: {
+          id: supplier.products.id,
+          label: supplier.products.label,
+          price: supplier.products.price,
+        },
+      });
+    }
+    if (supplier.id) {
+      Object.assign(supplierJson, { id: supplier.id });
+    }
+    return supplierJson;
+  }
 }
